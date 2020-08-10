c0b8ddcb9ab6f1c34059dc6b627f67353d324f94
Kesha Antonov <innokenty.longway@gmail.com>
2020-08-10 14:17:32 +0300

    make build

--- dist/cable.js
+++ dist/cable.js
@@ -1,1 +1,1 @@
-var Cable;export default Cable=class{constructor(n){this.channel=this.channel.bind(this),this.setChannel=this.setChannel.bind(this),this.channels=n}channel(n){return this.channels[n]}setChannel(n,e){return this.channels[n]=e}};
+var Cable;Cable=class{constructor(n){this.channel=this.channel.bind(this),this.setChannel=this.setChannel.bind(this),this.channels=n}channel(n){return this.channels[n]}setChannel(n,e){return this.channels[n]=e}};export default Cable;
